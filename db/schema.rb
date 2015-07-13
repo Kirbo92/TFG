ca3426cd7e08e565c3b5466ada09cd0662f47270
Miguel Ángel Fernández Carrillo <cociocibil@ono.com>
2015-07-13 19:30:59 +0200

    Creado modelo follower

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712153319) do
+ActiveRecord::Schema.define(version: 20150713172603) do
+
+  create_table "followers", force: :cascade do |t|
+    t.integer  "user_id",          limit: 4
+    t.integer  "user_followed_id", limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "content",    limit: 140, null: false
