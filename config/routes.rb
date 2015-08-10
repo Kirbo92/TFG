1f9d03a9ae8df020e6401d94a6e44d453b5990f3
Miguel Ángel Fernández Carrillo <cociocibil@ono.com>
2015-08-10 23:15:04 +0200

    Configuracion de idiomas

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   	end
   end
   resources :relationships, only: [:create, :destroy]
+  get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
 end
