2c17ff14d7566c2f1923288c2185fb80619de47e
Miguel Ángel Fernández Carrillo <cociocibil@ono.com>
2015-08-18 15:13:31 +0200

    Eliminado seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,35 +14,7 @@
 	User.create(username: 'prueba', email: 'prueba@red.ujaen.es', 
 				name: 'Prueba', password: '12345678')
 
-	Relationship.create(follower_id: 2, followed_id: 1)
-
-	Post.create(content: 'Primer mensaje del sistema microblogging', user_id: 1)
-	Post.create(content: 'Este es mi trabajo final de grado', user_id: 2)
-	Post.create(content: 'Los mensajes de profesores y administración será de este color, mientras que el de los alumnos será de color gris', user_id: 1)
-	Post.create(content: 'Las pruebas funcionan bien', user_id: 3)	
-	Post.create(content: 'Todo listo para el despliege en heroku', user_id: 2)
-	Post.create(content: 'Mañana empiezan las clases de #POO', user_id: 1)
-	Post.create(content: 'Probando el grupo de #TECNOWEB ', user_id: 1)
-	Post.create(content: 'No me gustan las clases de #OGE ', user_id: 2)
-	Post.create(content: 'Aburridas clases de #OGE', user_id: 3)
-
-
-
-	Group.create(name: 'Organización y gestión de empresas', tag: '#OGE', description: 'Grupo de teoría de mañana', user_admin: 1)
-	Group.create(name: 'Programación orientada a objetos', tag: '#POO', description: 'Grupo de práctica 10:30-12:30', user_admin: 1)
-	Group.create(name: 'Tecnologías basadas en la web', tag: '#TECNOWEB', description: 'Grupo de teoría de mañana', user_admin: 1)
-
-	Membership.create(user_id: 2, group_id: 2)
-	Membership.create(user_id: 2, group_id: 3)
-
-	Membership.create(user_id: 3, group_id: 2)
-	Membership.create(user_id: 3, group_id: 3)
-
-	Tag.create(post_id: 6, group_id: 2)
-	Tag.create(post_id: 7, group_id: 3)
-	Tag.create(post_id: 8, group_id: 1)
-	Tag.create(post_id: 9, group_id: 1)
-
+	
 
 
 
